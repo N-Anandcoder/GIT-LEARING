54bf689be809291e4fb17671dafa7bd400fed294
Anand <anandraodknb12h@gmail.com>
2024-08-19 13:04:03 +0530

    fll added

--- day3/iterations.js
+++ day3/iterations.js
@@ -1,9 +1,9 @@
 var arr = [1, 2, 3, 4, 5];
 
 // 1. For Loop:
-// for (var x = 0; x < arr.length; x += 2) {
-//   console.log(arr[x]);
-// }
+ for (var x = 0; x < arr.length; x++) {
+   console.log(arr[x]);
+ }
 
 // 2. forEach
 // var sum = 0;
@@ -28,17 +28,17 @@ var arr = [1, 2, 3, 4, 5];
 // }
 
 // Object
-var data = {
-  name: "Vishnu",
-  age: 25,
-  address: {
-    addressOne: "",
-  },
-};
+// var data = {
+//   name: "Vishnu",
+//   age: 25,
+//   address: {
+//     addressOne: "",
+//   },
+// };
 
 // 1. forin
 // 2. Object.keys(), Object.values()
 // console.log(data); // {}
 // console.log(Object.keys(data)); // ['name', 'age']
 // console.log(Object.values(data)); // ['Vishnu', 25]
-console.log(Object.entries(data));
+// console.log(Object.entries(data));
